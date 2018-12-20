ef7d89958d4598b203d99c9de2954d1de4f9d66d
Jayon Kim <jpkim921@gmail.com>
2018-12-20 01:59:45 +0000

    Automatically backed up by Learn

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,18 @@
+// function hideWhenClicked(event) {
+//   var $target = $(event.target);
+//   $target.hide();
+// }
+//
+// $('#hide_this').click(hideWhenClicked);
+
+
+
+
+
+
+let hideWhenClicked = (event) {
+  let clicked = $(event.target);
+  clicked.hide();
+}
+
+$['#hide_this').click(callbackFn)]
